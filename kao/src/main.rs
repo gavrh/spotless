084dd880f89d6f262e5e0c23a570784caa2cbbdf
gavin <96849112+gavrh@users.noreply.github.com>
2025-01-08 22:36:03 -0800

    basic webserver with templates, using axum and askama

--- kao/src/main.rs
+++ kao/src/main.rs
@@ -1,3 +1,41 @@
-fn main() {
-    println!("ctc - Kao (Frontend) v{}", env!("CARGO_PKG_VERSION"));
+use axum::{
+    http::StatusCode, response::{Html, IntoResponse}, routing::get, Router
+};
+use askama::Template;
+use chrono::{ DateTime, Local };
+
+#[derive(Template)]
+#[template(path = "index.html")]
+struct IndexTemplate {
+    footer: Option<FooterTemplate>
 }
+
+struct FooterTemplate {
+    time: String
+}
+
+async fn root() -> impl IntoResponse {
+    (
+        StatusCode::OK, 
+        Html(
+            IndexTemplate{
+                footer: Some(FooterTemplate {
+                    time: format!("{}", Local::now().format("%b %d, %Y (%H:%M)"))
+                })
+            }.to_string()
+        )
+    ).into_response()
+}
+
+#[tokio::main]
+async fn main() {
+    // tracing
+    tracing_subscriber::fmt::init();
+
+    let app = Router::new()
+        .route("/", get(root));
+
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
+}
+
