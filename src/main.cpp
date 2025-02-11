f5b773c08e5a0e8fb0ae8c7e3ba5481368d13169
gavin <96849112+gavrh@users.noreply.github.com>
2025-02-11 00:53:36 -0800

    add ftxui + basic setup

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,22 @@
+#include <ftxui/dom/elements.hpp>
+#include <ftxui/screen/screen.hpp>
 #include <iostream>
 
-int main(int argc, char* argv[]) {
-    std::cout << "Spotless, no dumb electon app! ;)" << "\n";
+int main(void) {
 
-    return 0;
+    ftxui::Element document =
+        ftxui::hbox({
+            ftxui::text("left") | ftxui::border,
+            ftxui::text("middle") | ftxui::border | ftxui::flex,
+            ftxui::text("right") | ftxui::border,
+        });
+
+    auto screen = ftxui::Screen::Create(
+        ftxui::Dimension::Full(),
+        ftxui::Dimension::Fit(document)
+    );
+    ftxui::Render(screen, document);
+    screen.Print();
+
+    return EXIT_SUCCESS;
 }
