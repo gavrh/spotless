02583030334f01c206d2799f9e031bbc2ddc6268
gavin <96849112+gavrh@users.noreply.github.com>
2025-02-11 10:17:48 -0800

    added nlohmann/json

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,15 @@
 #include <ftxui/dom/elements.hpp>
 #include <ftxui/screen/screen.hpp>
 #include <iostream>
+#include <iomanip>
+#include <nlohmann/json.hpp>
+
+using json = nlohmann::json;
 
 int main(void) {
+
+    std::cout << std::setw(4) << json::meta() << std::endl;
+
 
     ftxui::Element document =
         ftxui::hbox({
