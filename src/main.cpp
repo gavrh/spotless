fbe554b1718f4c8ac062fe4edcf0fbd04a007dcd
gavin <96849112+gavrh@users.noreply.github.com>
2025-02-14 04:34:09 -0800

    messing with libcurl

--- src/main.cpp
+++ src/main.cpp
@@ -3,8 +3,16 @@
 #include <iostream>
 #include <iomanip>
 #include <nlohmann/json.hpp>
+#include <curl/curl.h>
+#include <cstddef>
 
 using json = nlohmann::json;
+
+size_t write_callback(char *ptr, size_t size, size_t nmemb, std::string *data) {
+    size_t total_size = size * nmemb;
+    data->append(ptr, total_size);
+    return total_size;
+}
 
 int main(void) {
 
@@ -24,6 +32,28 @@ int main(void) {
     );
     ftxui::Render(screen, document);
     screen.Print();
+
+    CURL *curl;
+    CURLcode res;
+    std::string response_data;
+    curl_global_init(CURL_GLOBAL_DEFAULT);
+    curl = curl_easy_init();
+    if (curl) {
+        curl_easy_setopt(curl, CURLOPT_URL, "https://api.github.com/users/gavrh");
+        curl_easy_setopt(curl, CURLOPT_USERAGENT, "Spotless");
+        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_callback);
+        curl_easy_setopt(curl, CURLOPT_WRITEDATA, &response_data);
+
+        res = curl_easy_perform(curl);
+        if (res != CURLE_OK) {
+            std::cerr << "curl_easy_perform() failed: " << curl_easy_strerror(res) << std::endl;
+        } else {
+            json d = json::parse(response_data);
+            std::cout << d.dump(4) << std::endl;
+        }
+        curl_easy_cleanup(curl);
+    }
+    curl_global_cleanup();
 
     return EXIT_SUCCESS;
 }
