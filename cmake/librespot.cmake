4bd2fec48cba2ab5f2bf321676027cbc49dd5e61
gavin <96849112+gavrh@users.noreply.github.com>
2025-02-18 04:44:03 -0800

    correct the github repo link for librespot fetch

--- cmake/librespot.cmake
+++ cmake/librespot.cmake
@@ -1,6 +1,6 @@
 message(STATUS "Fetching librespot...")
 FetchContent_Declare(librespot
-    GIT_REPOSITORY https://github.com/gavrh/librespot-cpp
+    GIT_REPOSITORY https://github.com/gavrh/librespot-c
     GIT_TAG master
 )
 FetchContent_MakeAvailable(librespot)
