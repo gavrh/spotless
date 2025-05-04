a4eae1319393102b75544835464e8df3a0a1cb07
Gavin Holmes <gavin@gav.codes>
2025-05-03 20:29:41 -0700

    good changes to config

--- include/config.hpp
+++ include/config.hpp
@@ -1,15 +1,51 @@
 #ifndef SPOTLESS_CONFIG_HPP
 #define SPOTLESS_CONFIG_HPP
 
+#include <cstdint>
 #include <string>
-#include <unordered_map>
+#include <toml++/toml.hpp>
 
 namespace spotless {
 namespace config {
 
+// default config path
+constexpr const char* DEFAULT_PATH = "~/.config/spotless/config.toml";
+
+class ConfigOptions {
+
+public:
+    bool show_features = true;
+
+    ConfigOptions();
+    ConfigOptions(const toml::node_view<toml::node> &node);
+    ~ConfigOptions();
+
+};
+
+class ConfigTheme {
+
+public:
+    uint32_t main =         0x000000;
+    uint32_t secondary =    0xffffff;
+
+    ConfigTheme();
+    ConfigTheme(const toml::node_view<toml::node> &node);
+    ~ConfigTheme();
+
+};
+
 class Config {
+
 private:
-    std::unordered_map<std::string, std::string> colors;
+    std::string     path;
+    ConfigOptions   options;
+    ConfigTheme     theme;
+
+public:
+
+    Config();
+    Config(std::string path);
+    ~Config();
 
 };
 
--- src/config.cpp
+++ src/config.cpp
@@ -3,7 +3,31 @@
 namespace spotless {
 namespace config {
 
+ConfigOptions::ConfigOptions(const toml::node_view<toml::node> &node) {
+}
 
+ConfigTheme::ConfigTheme(const toml::node_view<toml::node> &node) {
+}
+
+Config::Config() {
+
+    this->path = config::DEFAULT_PATH;
+
+    toml::table config = toml::parse_file(this->path);
+
+    const toml::node_view<toml::node> &options = config["config"];
+    const toml::node_view<toml::node> &theme = config["theme"];
+
+    this->options = ConfigOptions(options);
+    this->theme = ConfigTheme(theme);
+
+}
+
+Config::Config(std::string path) {
+
+    this->path = path;
+
+}
 
 }
 }
