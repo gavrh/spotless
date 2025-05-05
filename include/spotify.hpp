2a19f9e2fd56ce85a59eea70a904598cc1b16d91
Gavin Holmes <gavin@gav.codes>
2025-05-04 23:32:13 -0700

    starting to build out spotify classes

--- include/spotify.hpp
+++ include/spotify.hpp
@@ -1,32 +1,82 @@
 #ifndef SPOTLESS_SPOTIFY_HPP
 #define SPOTLESS_SPOTIFY_HPP
 
+#include <cache.hpp>
 #include <librespot/playback.h>
+#include <queue>
+#include <string>
+#include <vector>
 
 namespace spotless {
 namespace spotify {
 
-class User {
+static constexpr const char* SPOTIFY_CLIENT_ID = "65b708073fc0480ea92a077233ca87bd";
+static constexpr const char* SPOTIFY_REDIRECT_URI = "http://127.0.0.1:8989/login";
 
-private:
-    char* token;
+enum SpotifyItemType {
+    TRACK,
+};
+
+class SpotifyItem {
 
 public:
-    
+    std::string                 id;
+    SpotifyItemType             type;
+    std::string                 artist;
+    std::vector<std::string>    features;
 
-    void refresh_token();
+    SpotifyItem();
+    ~SpotifyItem();
+
+};
+
+class Context {
+
+public:
+    SpotifyItem             current;
+    uint32_t                position_ms;
+    std::queue<SpotifyItem> queue;
+
+    Context();
+    ~Context();
+
+};
+
+class User {
+
+public:
+    std::string access_token;
+    std::string refresh_token;
+
+    User();
+    ~User();
+
+
 };
 
 class Spotify {
 
 private:
-    User    user;
-    Player* player;
-    Mixer*  mixer;
+    Context         context;
+    User            user;
+    Player*         player;
+    PlayerChannel*  player_channel;
+    Mixer*          mixer;
 
 public:
-    Spotify();
+    Spotify(cache::Cache &cache);
     ~Spotify();
+
+    void HandleEvent(PlayerEvent* player_event);
+    void Preload();
+    void Preload(std::string spotify_uri);
+    void Load();
+    void Load(
+        std::string spotify_uri,
+        bool        start_playing,
+        uint32_t    position_ms
+    );
+    uint32_t PositionMs();
 
 };
 
--- src/spotify.cpp
+++ src/spotify.cpp
@@ -1,18 +1,57 @@
+#include <librespot/oauth.h>
 #include <spotify.hpp>
-#include <iostream>
 
 namespace spotless {
 namespace spotify {
 
-void User::refresh_token() {
-    std::cout << "spotless" << std::endl;
-    std::cout << std::endl;
-    return;
+SpotifyItem::SpotifyItem() {}
+SpotifyItem::~SpotifyItem() {}
+
+Context::Context() {}
+Context::~Context() {}
+
+User::User() {}
+User::~User() {}
+
+Spotify::Spotify(cache::Cache &cache) {
+    OAuthBuilder* oauth = oauth_builder_new(SPOTIFY_CLIENT_ID, SPOTIFY_REDIRECT_URI);
+    oauth_builder_auto_open(oauth);
+    oauth_builder_add_scope(oauth, OAUTH_SCOPE_ALL);
+    OAuth* auth = oauth_build(oauth);
+
+    const char* access = oauth_access_token(auth);
+    Credentials* creds = credentials_new(access);
+    Session* session = session_new(session_config_default());
+    session_connect(session, creds);
+
+    this->mixer = mixer_new(mixer_config_default(), "softvol");
+    this->player = player_new(player_config_default(), session, this->mixer, "pulseaudio");
+
+    mixer_set_volume(mixer, mixer_get_volume(this->mixer) * 0.75);
+    player_load(this->player, "spotify:track:55rXApL8KBessVIpJpH4Fw", true, 45000);
+
+    this->player_channel = player_channel_get(this->player);
+
+}
+Spotify::~Spotify() {}
+
+void Spotify::HandleEvent(PlayerEvent* player_event) {
+
 }
 
-Spotify::Spotify() {}
+void Spotify::Preload() {}
+void Spotify::Preload(std::string spotify_uri) {
+    player_preload(this->player, spotify_uri.c_str());
+}
 
-Spotify::~Spotify() {}
+void Spotify::Load() {}
+void Spotify::Load(
+    std::string spotify_uri,
+    bool        start_playing,
+    uint32_t    position_ms
+) {
+    player_load(this->player, spotify_uri.c_str(), start_playing, position_ms);
+}
 
 }
 }
