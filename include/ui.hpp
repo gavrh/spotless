62e175683f359e4a27a7dda4357f876b97a342cc
Gavin Holmes <gavin@gav.codes>
2025-05-04 23:32:41 -0700

    figuring out how im gonna do the ui

--- include/ui.hpp
+++ include/ui.hpp
@@ -3,17 +3,22 @@
 
 #include <cache.hpp>
 #include <config.hpp>
+#include <ftxui/component/component.hpp>
+#include <ftxui/component/component_base.hpp>
 #include <spotify.hpp>
+#include <vector>
 
 namespace spotless {
 namespace ui {
 
+
 class App {
 
 private:
-    cache::Cache        &cache;
-    config::Config      &config;
-    spotify::Spotify    &spotify;
+    cache::Cache                    &cache;
+    config::Config                  &config;
+    spotify::Spotify                &spotify;
+    std::vector<ftxui::Component>   views;
     
 public:
     
